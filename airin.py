b52c0fcfdc77f56c7872d3688ad179a621444bf2
Kresna Jenie <kresnajenie@Kresnas-MacBook-Air.local>
2017-05-25 11:22:04 +0700

    Baru mulai

--- airin.py
+++ airin.py
@@ -1,6 +1,6 @@
 
 import speech_recognition  as sr
-
+import webbrowser
 # Record Audio
 r = sr.Recognizer()
 with sr.Microphone() as source:
@@ -12,8 +12,15 @@ try:
 	# for testing purposes, we're just using the default API key
 	# to use another API key, use 'r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_KEY)'
 	# instead of 'recognize_google(audio)'
-	print("You said: " + r.recognize_google(audio))
+	perintah = r.recognize_google(audio)
 except sr.UnknownValueError:
 	print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
 	print("Could not request results from Google Speech Recognition service; {0}".format(e))
+
+if perintah == "open":
+	webbrowser.open("http://www.tokopedia.com")
+elif perintah == "store":
+	webbrowser.open("http://www.tokopedia.com/izakjenie")
+elif perintah == "add product":
+	webbrowser.open("https://www.tokopedia.com/product-add.pl?ref=addpdside")
---
+++ browser.py
@@ -0,0 +1,3 @@
+from selenium import webdriver
+ff = webdriver.Firefox()
+ff.get("https://www.tokopedia.com")
